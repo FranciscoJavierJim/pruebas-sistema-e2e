e04709001ee6906e13d63c878899393fb61099dc
FranciscoJavierJim <225529106+FranciscoJavierJim@users.noreply.github.com>
2025-11-12 15:51:01 +0100

    correcciones

--- cypress/support/pages/POM_todo_page.js
+++ cypress/support/pages/POM_todo_page.js
@@ -1,12 +1,12 @@
 export class POMTodoPage {  // para importar la clase a los test
 
   elements = {  // agrupa funciones que devuelven selectores en cypress
-    input: () => cy.get('[data-testid="text-input"]'),  // selecciona el campo para poder añadir tareas
+    newTaskInput: () => cy.get('[data-testid="text-input"]'),  // selecciona el campo para poder añadir tareas
     taskItem: (text) => cy.contains('[data-testid="todo-item"]', text),  // selecciona un item de la lista por contenido
-    taskLabel: (text) => cy.contains('[data-testid="todo-item-label"]', text), // selecciona solo por la etiqueta
-    taskToggle: (text) => cy.contains('.todo-list li', text).find('[data-testid="todo-item-toggle"]'),  // selecciona el check de la tarea. localiza primero el li que contiene el texto y luego busca el check
+    taskLabel: () => cy.get('[data-testid="todo-item-label"]'), // selecciona solo por la etiqueta
+    taskCheckBox: (text) => cy.contains('.todo-list li', text).find('[data-testid="todo-item-toggle"]'),  // selecciona el check de la tarea. localiza primero el li que contiene el texto y luego busca el check
     deleteButton: (text) => cy.contains('.todo-list li', text).find('.destroy'), // selecciona el boton para eliminar una tarea
-    footerFilter: (option) => cy.get('[data-testid="footer-navigation"]').contains(option), // selecciona los filtros por texto
+    filterOption: (option) => cy.get('[data-testid="footer-navigation"]').contains(option), // selecciona los filtros por texto
   };
 
   visit() {
@@ -14,19 +14,19 @@ export class POMTodoPage {  // para importar la clase a los test
   }
 
   addTask(taskName) {
-    this.elements.input().should('be.visible').type(`${taskName}{enter}`);
+    this.elements.newTaskInput().should('be.visible').type(`${taskName}{enter}`);
   }
 
   markTaskCompleted(taskName) {
-    this.elements.taskToggle(taskName).check({ force: true });
+    this.elements.taskCheckBox(taskName).check({ force: true });
   }
 
   unmarkTask(taskName) {
-    this.elements.taskToggle(taskName).uncheck({ force: true });
+    this.elements.taskCheckBox(taskName).uncheck({ force: true });
   }
 
   editTask(oldName, newName) {
-    this.elements.taskLabel(oldName).dblclick({ force: true });
+    this.elements.taskLabel().filter(':contains("'+oldName+'")').dblclick({ force: true });
     cy.focused().clear().type(`${newName}{enter}`); // con cy.focused() obitene el input que quedo activo tras hacer doble click, luego borra y escribe la nueva tarea
   }
 
@@ -35,11 +35,11 @@ export class POMTodoPage {  // para importar la clase a los test
   }
 
   filter(option) {
-    this.elements.footerFilter(option).click();   // opciones: 'All', 'Active', 'Completed'
+    this.elements.filterOption(option).click();   // opciones: 'All', 'Active', 'Completed'
   }
 
   shouldSeeTask(taskName) {
-    this.elements.taskLabel(taskName).should('exist');
+    this.elements.taskLabel().filter(':contains("'+taskName+'")').should('exist');
   }
 
   shouldNotSeeTask(taskName) {
@@ -47,11 +47,11 @@ export class POMTodoPage {  // para importar la clase a los test
   }
 
   shouldBeCompleted(taskName) {
-    this.elements.taskToggle(taskName).should('be.checked');
+    this.elements.taskCheckBox(taskName).should('be.checked');
   }
 
   shouldBeActive(taskName) {
-    this.elements.taskToggle(taskName).should('not.be.checked');
+    this.elements.taskCheckBox(taskName).should('not.be.checked');
   }
 
   shouldHaveTasks(expectedNames) {
